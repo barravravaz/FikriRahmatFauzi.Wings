a32bddae76f955da6edec86f8ed1c446e355b661
barravravaz <123834432+barravravaz@users.noreply.github.com>
2023-01-29 02:18:40 +0700

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<footer class="main-footer">
+    <div class="container">
+      <strong>Copyright &copy; 2023  By <a href="#">Fikri Rahmat Fauzi</a></strong>
+    </div>
+</footer>
---
+++ includes/scripts.php
@@ -0,0 +1,77 @@
+<script src="bower_components/jquery/dist/jquery.min.js"></script>
+<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+<script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<script src="bower_components/fastclick/lib/fastclick.js"></script>
+<script src="dist/js/adminlte.min.js"></script>
+<script src="bower_components/ckeditor/ckeditor.js"></script>
+
+<script>
+  $(function () {
+   
+    $('#example1').DataTable()
+    
+    CKEDITOR.replace('editor1')
+  });
+</script>
+
+<script src="magnify/magnify.min.js"></script>
+<script>
+$(function(){
+	$('.zoom').magnify();
+});
+</script>
+
+<script>
+$(function(){
+  $('#navbar-search-input').focus(function(){
+    $('#searchBtn').show();
+  });
+
+  $('#navbar-search-input').focusout(function(){
+    $('#searchBtn').hide();
+  });
+
+  getCart();
+
+  $('#productForm').submit(function(e){
+  	e.preventDefault();
+  	var product = $(this).serialize();
+  	$.ajax({
+  		type: 'POST',
+  		url: 'cart_add.php',
+  		data: product,
+  		dataType: 'json',
+  		success: function(response){
+  			$('#callout').show();
+  			$('.message').html(response.message);
+  			if(response.error){
+  				$('#callout').removeClass('callout-success').addClass('callout-danger');
+  			}
+  			else{
+				$('#callout').removeClass('callout-danger').addClass('callout-success');
+				getCart();
+  			}
+  		}
+  	});
+  });
+
+  $(document).on('click', '.close', function(){
+  	$('#callout').hide();
+  });
+
+});
+
+function getCart(){
+	$.ajax({
+		type: 'POST',
+		url: 'cart_fetch.php',
+		dataType: 'json',
+		success: function(response){
+			$('#cart_menu').html(response.list);
+			$('.cart_count').html(response.count);
+		}
+	});
+}
+</script>
